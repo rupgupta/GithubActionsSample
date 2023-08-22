d073a10542a44c0c7c0702c441efcad9527c9ee2
rupgupta <rupgupta@deloitte.com>
2023-08-22 12:19:02 +0530

    Initial Commit

---
+++ GithubActionsSample/GithubActionsSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubActionsSampleApp.swift
+//  GithubActionsSample
+//
+//  Created by Gupta, Rupali on 22/08/23.
+//
+
+import SwiftUI
+
+@main
+struct GithubActionsSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
