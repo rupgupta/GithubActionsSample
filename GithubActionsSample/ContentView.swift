db7ce2fa9d5fae8f637b4aa48e18dc1a271db059
Rupali Gupta <rupgupta@deloitte.com>
2023-08-25 11:09:48 +0530

    Project Set up remote

--- GithubActionsSample/ContentView.swift
+++ GithubActionsSample/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+            Text("We will be working on Github Actions here.")
         }
         .padding()
     }
